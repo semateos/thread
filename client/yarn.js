99106277bf7a78a5f1b27ca5f2857ec1116cdd09
Sam Mateosian <sam@bigroomstudios.com>
2013-01-21 15:37:49 -0500

    lots of work on selections and improved performance on ipad

--- client/yarn.js
+++ client/yarn.js
@@ -1,49 +1,6 @@
-
-Paths = new Meteor.Collection("paths");
-
 paper.install(window);
 
-$(function(){
-	
-	var shapes = Paths.find({})
-	shapes.forEach(function(shape) {
-		console.log('shape: ' + shape.d);	
-	})	
-})
-
-
-/*
-function drawShapes() {
-	var shapes = Paths.find({})
-	shapes.forEach(function(shape) {
-		
-		})
-}
-
-var startUpdateListener = function() {
-// Function called each time 'Shapes' is updated.
-		
-var redrawCanvas = function() {
-var context = new Meteor.deps.Context()
-context.on_invalidate(redrawCanvas) // Ensures this is recalled for each update
-		context.run(function() {
-			drawShapes()
-		})
-	}
-	redrawCanvas()
-}
-
-Meteor.startup(function() {
-	startUpdateListener()
-})
-
-
-*/
-
-Template.canvas.paths = function () {
-	
-	return Paths.find();
-};
+Paths = new Meteor.Collection("paths");
 
 Template.header.rendered = function () {
 	
@@ -53,33 +10,244 @@ Template.header.rendered = function () {
 	
 	var svg = canvas.svg('get');
 	
-	svg.circle(40, 40, 25, {fill: 'none', stroke: 'red', strokeWidth: 1});
-	
-	//var path = svg.createPath(); 
-	
+	svg.circle(30, 30, 20, {fill: 'none', stroke: 'red', strokeWidth: 1});
+
 
 };
 
 Template.header.events({
 	
-	'mouseover #logo': function(event){}
+	'click #logo': function(event){
+		projects[1].activeLayer.removeChildren();
+		Paths.remove({}); 
+	}
 });
 
 
-
 Template.canvas.rendered = function () {
-	
+
 	paper.setup('canvas');
+	paper.setup('canvas2');
+
+	projects[0].activate();
 
 	console.log('canvas rendered');
+
+
+	// Create a simple drawing tool:
+	var draw_tool = new Tool();
+	var path;
 	
-	if (! self.drawCanvas) {
+	// Define a mousedown and mousedrag handler
+	draw_tool.onMouseDown = function(event) {
 		
+		projects[0].activate();
+
+		event.preventDefault();
+
+		path = new Path();
+		 
+		path.strokeColor = 'red';
+
+		path.add(event.point);
+
+		projects[0].view.draw();
+	}
+
+	draw_tool.onMouseDrag = function(event) {
+		
+		projects[0].activate();
+
+		//event.preventDefault();
+
+		path.add(event.point);
+
+		projects[0].view.draw();
+	}
+
+	draw_tool.onMouseUp = function(event) {
+		
+		projects[0].activate();
+
+		// When the mouse is released, simplify it:
+				
+		path.simplify(10);
+		
+		var p = path.exportSvg();
+		
+		var d = $(p).attr('d');
+		
+		//console.log(d);
+		
+		Paths.insert({d:d}, function(err, id){
+			projects[0].activeLayer.removeChildren();
+			projects[0].view.draw();
+			console.log(id);
+		});
+
+		
+	}
+
+	projects[1].activate();
+
+	// Create a simple drawing tool:
+	var select_tool = new Tool();
+	var hitOptions = {
+		segments: true,
+		stroke: true,
+		fill: true,
+		tolerance: 5
+	};
+
+	
+
+	var segment, path;
+	var movePath = false;
+	var selected_item = false;
+	var selected_list = new Array();
+
+
+	
+	select_tool.onMouseDown = function(event) {
+			
+		event.preventDefault();
+
+		segment = path = null;
+
+		var hitResult = project.hitTest(event.point, hitOptions);
+
+		if (hitResult && hitResult.item){
+
+			if(selected_item == hitResult.item){
+
+				console.log('same_item');
+
+				if (event.modifiers.shift) {
+
+					if (hitResult.type == 'segment') {
+							hitResult.segment.remove();
+					};
+					return;
+				}
+
+				if (hitResult) {
+					path = hitResult.item;
+					selected_item = path;
+					if (hitResult.type == 'segment') {
+							segment = hitResult.segment;
+					} else if (hitResult.type == 'stroke') {
+							var location = hitResult.location;
+							segment = path.insert(location.index + 1, event.point);
+							path.smooth();
+					}
+				}
+
+			}else{
+
+				if(selected_item){
+					self.updatePath(selected_item);
+				}
+
+				console.log('different_item');
+
+				project.activeLayer.selected = false;
+
+				selected_item = hitResult.item;
+
+				selected_item.selected = true;
+
+				console.log(selected_item._id);
+
+			}
+		}else{
+
+			if(selected_item){
+				self.updatePath(selected_item);
+			}
+
+			project.activeLayer.selected = false;
+
+			selected_item = false;
+		}
+		
+
+		/*
+			segment = path = null;
+			var hitResult = project.hitTest(event.point, hitOptions);
+
+			if (event.modifiers.shift) {
+					if (hitResult.type == 'segment') {
+							hitResult.segment.remove();
+					};
+					return;
+			}
+
+			if (hitResult) {
+					path = hitResult.item;
+					selected_item = path;
+					if (hitResult.type == 'segment') {
+							segment = hitResult.segment;
+					} else if (hitResult.type == 'stroke') {
+							var location = hitResult.location;
+							segment = path.insert(location.index + 1, event.point);
+							path.smooth();
+					}
+			}
+			movePath = hitResult.type == 'fill';
+			if (movePath)
+					project.activeLayer.addChild(hitResult.item);
+			*/
+
+
+	}
+	
+
+	select_tool.onMouseMove = function(event) {
+		 
+		projects[1].activate();
+
+		if(!selected_item){
+
+			console.log('move');
+
+				var hitResult = project.hitTest(event.point, hitOptions);
+				
+				project.activeLayer.selected = false;
+				
+				selected_item = false;
+
+				if (hitResult && hitResult.item){
+					 	
+						hitResult.item.selected = true;
+				}
+		}
+	}
+
+	
+
+	select_tool.onMouseDrag = function(event) {
+
+		event.preventDefault();
+
+			if (segment) {
+					segment.point = event.point;
+					path.smooth();
+			}
+
+			if (movePath)
+					path.position += event.delta;
+	}
+
+
+	var path_pointers = {}
+
+	if (! self.drawCanvas) {
+	
 		//redraw whenever the data changes
-		self.drawCanvas = Meteor.autorun(function() {
+		self.drawCanvas = Meteor.subscribe("allpaths", function() {
 			
-			//paper.setup('canvas');
-			
+			projects[1].activate();
+
 			var path = new Path();
 			
 			path.strokeColor = 'red';
@@ -92,65 +260,112 @@ Template.canvas.rendered = function () {
 			
 			path.remove();
 			
-			project.activeLayer.removeChildren();
 			
-			var paths = Paths.find().fetch(); 
+			projects[1].activeLayer.removeChildren();
+
+			//layer1.removeChildren();
 			
-			paths.forEach(function (path_data) {
-				
-				var d = path_data.d;
-		
-				$(p).attr('d', d);
-			
-				$(p).attr('style', 'fill: none; stroke: red; stroke-width: 1');
+			var paths = Paths.find();
+
+			var handle = paths.observe({
+
+				added: function (path_data) {
 					
-				path = project.importSvg($(p)[0]);
-				
-				//console.log(path_data.d);
-			});
+					console.log('added ' + path_data.d);
+
+					projects[1].activate();
+
+					var d = path_data.d;
 			
-			paper.view.draw();
+					$(p).attr('d', d);
+				
+					$(p).attr('style', 'fill: none; stroke: red; stroke-width: 1');
+						
+					var path = project.importSvg($(p)[0]);
+					
+					path._id = path_data._id;
+
+					path_pointers[path_data._id] = path;
+
+					projects[1].view.draw();
+
+				},
+
+				changed: function(path_data){
+
+					console.log('updated ' + path_data.d);
+
+					projects[1].activate();
+
+					var path = path_pointers[path_data._id];
+
+					path.remove();
+
+					var d = path_data.d;
+			
+					$(p).attr('d', d);
+				
+					$(p).attr('style', 'fill: none; stroke: red; stroke-width: 1');
+						
+					path = project.importSvg($(p)[0]);
+					
+					path._id = path_data._id;
+
+					path_pointers[path_data._id] = path;
+
+					projects[1].view.draw();
+
+
+				},
+
+				removed: function (path_data) {
+					
+					console.log('added ' + path_data.d);
+					
+				}
+			});
+
+			
 		});
 	}
 	
+	self.updatePath = function(path){
 
-	// Create a simple drawing tool:
-	var tool = new Tool();
-	var path;
-	
-	// Define a mousedown and mousedrag handler
-	tool.onMouseDown = function(event) {
-
-		path = new Path();
-		 
-		path.strokeColor = 'red';
-
-		path.add(event.point);
-	}
-
-	tool.onMouseDrag = function(event) {
-		
-		path.add(event.point);
-	}
-
-	tool.onMouseUp = function(event) {
-		
-		// When the mouse is released, simplify it:
-				
-		path.simplify(10);
-		
 		var p = path.exportSvg();
 		
 		var d = $(p).attr('d');
 		
+		var id = path._id;
+
 		//console.log(d);
 		
-		Paths.insert({d:d}, function(err, id){
-			console.log(id);
-		});
+		Paths.update({_id:id}, {$set: {d:d}}, function(err){});
 	}
-	
-};
+
+	$('#tools li').on('click', function(event){
+
+		var current_tool = $(this);
+
+		var tool_id = current_tool.attr('id');
+
+		$('#tools li').removeClass('selected');
+		
+		current_tool.addClass('selected');
+
+		if(tool_id == 'select_tool'){
+
+			$('#canvas').hide();
+
+		}else{
+
+			$('#canvas').show();
+			
+		}
+
+		eval(tool_id + '.activate()');
+	})
+
+}
 
 
 Accounts.ui.config({
--- server/publish.js
+++ server/publish.js
@@ -1,1 +1,5 @@
 Paths = new Meteor.Collection("paths");
+
+Meteor.publish("allpaths", function () {
+  return Paths.find({});
+});
