2ae30e6311d3679db8b93302ee26adf2952e6be2
Sam Mateosian <sam@bigroomstudios.com>
2013-01-21 10:38:46 -0500

    changed reactive canvas handling

--- client/yarn.js
+++ client/yarn.js
@@ -3,11 +3,47 @@ Paths = new Meteor.Collection("paths");
 
 paper.install(window);
 
+$(function(){
+	
+	var shapes = Paths.find({})
+	shapes.forEach(function(shape) {
+		console.log('shape: ' + shape.d);	
+	})	
+})
+
+
+/*
+function drawShapes() {
+	var shapes = Paths.find({})
+	shapes.forEach(function(shape) {
+		
+		})
+}
+
+var startUpdateListener = function() {
+// Function called each time 'Shapes' is updated.
+		
+var redrawCanvas = function() {
+var context = new Meteor.deps.Context()
+context.on_invalidate(redrawCanvas) // Ensures this is recalled for each update
+		context.run(function() {
+			drawShapes()
+		})
+	}
+	redrawCanvas()
+}
+
+Meteor.startup(function() {
+	startUpdateListener()
+})
+
+
+*/
+
 Template.canvas.paths = function () {
 	
 	return Paths.find();
 };
-
 
 Template.header.rendered = function () {
 	
@@ -34,6 +70,49 @@ Template.header.events({
 Template.canvas.rendered = function () {
 	
 	paper.setup('canvas');
+
+	console.log('canvas rendered');
+	
+	if (! self.drawCanvas) {
+		
+		//redraw whenever the data changes
+		self.drawCanvas = Meteor.autorun(function() {
+			
+			//paper.setup('canvas');
+			
+			var path = new Path();
+			
+			path.strokeColor = 'red';
+			path.add(new Point(0,0));
+			path.add(new Point(0,5));
+			path.add(new Point(10,10));
+			path.smooth();
+			
+			var p = path.exportSvg();
+			
+			path.remove();
+			
+			project.activeLayer.removeChildren();
+			
+			var paths = Paths.find().fetch(); 
+			
+			paths.forEach(function (path_data) {
+				
+				var d = path_data.d;
+		
+				$(p).attr('d', d);
+			
+				$(p).attr('style', 'fill: none; stroke: red; stroke-width: 1');
+					
+				path = project.importSvg($(p)[0]);
+				
+				//console.log(path_data.d);
+			});
+			
+			paper.view.draw();
+		});
+	}
+	
 
 	// Create a simple drawing tool:
 	var tool = new Tool();
@@ -64,61 +143,15 @@ Template.canvas.rendered = function () {
 		
 		var d = $(p).attr('d');
 		
-		console.log(d);
+		//console.log(d);
 		
 		Paths.insert({d:d}, function(err, id){
 			console.log(id);
 		});
 	}
 	
-	
-	//var d = 'M319,276 c-21.74262,0 -67.62168,50.45878 -77,66 c-13.09519,21.7006 -20.46856,49.2799 -24,74 c-2.25769,15.80383 -7.87183,51.12817 7,66 c24.17337,24.17337 68.00358,28.99809 98,13 c3.63614,-1.93927 47.17752,-28.74771 37,-38 c-7.12855,-6.4805 -30.45275,-4.51969 -35,-4 c-35.58043,4.06634 -34.55097,64.00364 -30,86 c3.02404,14.61619 11.09586,27.76706 18,41 c44.07041,84.46829 94.90192,138.17053 183,173';
-	
-	var path = new Path();
-	
-	path.strokeColor = 'red';
-	
-	path.add(new Point(0,0));
-	path.add(new Point(0,5));
-	path.add(new Point(10,10));
-	path.smooth();
-
-	var p = path.exportSvg();
-	
-	path.remove();
-				
-	var paths = Template.canvas.paths();
-
-	paths.forEach(function (path_data) {
-		
-		var d = path_data.d;
-		
-		$(p).attr('d', d);
-	
-		$(p).attr('style', 'fill: none; stroke: red; stroke-width: 1');
-			
-		path = project.importSvg($(p)[0]);		
-	});
-		
-	//path.fullySelected = true;
-		
-	paper.view.draw();	
-	
 };
 
-/*
-Template.canvas.events({
-	
-	'mousemove #canvas': function(e){canvas.draw(e)},
-	'touchmove #canvas': function(e){canvas.draw(e)},
-	
-	'mousedown #canvas': function(e){canvas.drawStart(e)},
-	'touchstart #canvas': function(e){canvas.drawStart(e)},
-	
-	'mouseup #canvas': function(e){canvas.drawEnd(e)},
-	'touchend #canvas': function(e){canvas.drawEnd(e)},
-});
-*/
 
 Accounts.ui.config({
 	requestPermissions: {
